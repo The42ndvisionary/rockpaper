876f9951d63c4533b20fed2c2a1f783ad6b4d62e
Shashwat <shashwatvirat5127@gmail.com>
2022-09-12 23:45:47 +0530

    single variable rock paper impolemented

--- app.js
+++ app.js
@@ -1,61 +1,30 @@
-
-function playround(playerselection,computerchoice){
-
-
-function playerselection(){
-    const a = prompt("choose rock paper or scissor");
-    const b = a.toLowerCase();
-    if (b === "rock"){
-        return "rock"
-    }
-    else if (b==="paper"){
-        return "paper"
-    }
-    else if (b==="scissor"){
-        return "scissor"
-    }
-    }
- 
-    function computerchoice(){
-        const random = Math.floor(Math.random() * 3);
-        if (random===0){
-            return "rock"
-        }
-        else if (random===1){
-            return "paper"
-        }
-        else if (random===2){
-            return "scissor"
-        }
-    }
-
-    if (playerselection() === "rock" &&  computerchoice() ==="rock"){
-        return "it is a tie comp chose rock"
-    }
-    else if (playerselection() === "rock" && computerchoice()=== "paper"){
-        return "player loses comp chose paper"
-    }
-    else if (playerselection() === "rock" && computerchoice()=== "scissor"){
-        return "player wins comp chose scissor"
-    }
-    else if (playerselection() === "paper" && computerchoice()=== "scissor"){
-        return "player loses comp chose scissor"
-    }
-    else if (playerselection() === "paper" && computerchoice()=== "rock"){
-        return "player wins comp chose rock"
-    }
-    else if (playerselection() === "scissor" && computerchoice()=== "paper"){
-        return "player loses comp chose paper"
-    }
-    else if (playerselection() === "scissor" && computerchoice()=== "rock"){
-        return "player loses comp chose rock"
-    }
-    else if (playerselection() === "paper" &&  computerchoice() ==="paper"){
-        return "it is a tie scissor paper"
-    }
-    else if (playerselection() === "scissor" &&  computerchoice() ==="scissor"){
-        return "it is a tie scissor scissor"
-    }
+const computerplay = () => {
+const arr0fChoices = ["rock","paper","scissor"]
+const randomnum =  Math.floor(Math.random() * 3)
+const compchoice = (arr0fChoices[randomnum])
+return compchoice
 }
 
-console.log(playround());
+const playround = (playerselection,computerelection) => {
+    if (playerselection === computerselection) {
+        return "you both tied, both chose " + playerselection
+    }
+    else if (playerselection === "rock" && computerselection === "paper"){
+        return  "player lost comp chose paper"
+    }
+    else if  (playerselection === "paper" && computerselection === "scissor"){
+        return  "player lost comp chose scissor"}
+        else if  (playerselection === "scissor" && computerselection === "rock"){
+            return  "player lost comp chose rock"}
+            else if  (playerselection === "scissor" && computerselection === "paper"){
+                return  "player won comp chose paper"}
+                if  (playerselection === "rock" && computerselection === "scissor"){
+                    return  "player won comp chose scissor"}
+                    if  (playerselection === "scissor" && computerselection === "paper"){
+                        return  "player won comp chose paper"}
+}
+
+
+const playselection = "rock";
+const computerselection = computerplay();
+console.log(playround(playselection,computerselection));
